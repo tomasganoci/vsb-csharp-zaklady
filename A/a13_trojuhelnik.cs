4268d0d0325e36436ff91afce241636e64d413ee
Tomáš Gánoci <tomas.ganoci@gmail.com>
2024-12-09 02:09:51 +0100

    přejmenovaní

---
+++ A/a13_trojuhelnik.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A {
+    internal class a13_trojuhelnik {
+        public static void Mainx() {
+            Console.WriteLine("A13_trojuhelnik");
+        }
+    }
+}
