a3e6a6c29a2ab272e8424f97caa5a3318ce7e1fa
Tomáš Gánoci <tomas.ganoci@gmail.com>
2024-12-23 23:23:03 +0100

    dodělání konverze příkladů D z VB na C#

--- D/d08_zadavani_ascii_znaku.cs
+++ D/d08_zadavani_ascii_znaku.cs
@@ -7,86 +7,102 @@ using System.Threading.Tasks;
 using Microsoft.VisualBasic.FileIO;
 
 namespace D {
+
     internal class d08_zadavani_ascii_znaku {
-        
 
-//8) Varianta příkladu ze skupiny b: Uživatel opakovaně zadá znak, program vždy odpoví, zda se jedná o písmeno, číslici nebo jiný znak.
-//Zadávání končí zadáním např. nuly (nebo to pro změnu zkuste ukončit nějakým jiným znakem).
-//Řešte pomocí } else { if.
-//Moje poznámky: skoro stejný postup
+        public static void Mainx() {
+            //8) Varianta příkladu ze skupiny b: Uživatel opakovaně zadá znak, program vždy odpoví, zda se jedná o písmeno, číslici nebo jiný znak.
+            //Zadávání končí zadáním např. nuly (nebo to pro změnu zkuste ukončit nějakým jiným znakem).
+            //Řešte pomocí } else { if.
+            //Moje poznámky: skoro stejný postup
+            //Moje poznámky C#: musím opravit, když je vstup prázdný.
 
-    zadanyZnak string, zadaneZnaky string
+            string zadaneZnaky;
+            char zadanyZnak;
 
-
-    zadaneZnaky = \"
-    Do
-    zadanyZnak = Console.WriteLine(Zadejte znak a já ti napíšu o jaký typ znaku se jedná.\nUkončete znakem \";\"")
-    zadaneZnaky = zadaneZnaky + zadanyZnak + Chr(10)
-    if zadanyZnak<> ";" {
-        if (zadanyZnak >= "a"  && zadanyZnak <= "z") {
-          Console.WriteLine("Znak je malé písmeno"
-        } else {if(zadanyZnak >= "A"  && zadanyZnak <= "Z") {
-          Console.WriteLine("Znak je velké písmeno"
-        } else {if(zadanyZnak >= "0"  && zadanyZnak <= "9") {
-          Console.WriteLine("Znak je číslo"
-        } else {
-          Console.WriteLine("Znak je jiný znak"
+            zadaneZnaky = "";
+            do {
+                Console.WriteLine("Zadejte znak a já ti napíšu o jaký typ znaku se jedná.\nUkončete znakem \";\"");
+                zadanyZnak = Convert.ToChar(Console.ReadLine());
+                zadaneZnaky += $"{zadanyZnak}\n";
+                if (zadanyZnak != ';') {
+                    if (zadanyZnak >= 'a' && zadanyZnak <= 'z') {
+                        Console.WriteLine("Znak je malé písmeno");
+                    }
+                    else if (zadanyZnak >= 'A' && zadanyZnak <= 'Z') {
+                        Console.WriteLine("Znak je velké písmeno");
+                    }
+                    else if (zadanyZnak >= '0' && zadanyZnak <= '9') {
+                        Console.WriteLine("Znak je číslo");
+                    }
+                    else {
+                        Console.WriteLine("Znak je jiný znak");
+                    }
+                }
+            }
+            while (zadanyZnak != ';');
+            Console.WriteLine($"Zadané znaky:\n{zadaneZnaky}");
         }
-    }
-    Loop Until zadanyZnak = ";"
-    Console.WriteLine("Zadané znaky:" + Chr(10) + zadaneZnaky)
 
+        public static void Mainx_reseni() {
+            //Řešení:
 
-//Řešení:
+            //d08: Varianta příkladu ze skupiny b: Uživatel opakovaně zadá znak, program vždy odpoví, zda se jedná o písmeno,
+            //číslici nebo jiný znak. Zadávání končí zadáním prázdného znaku (tedy jen ENTER). Řešte pomocí } else { if.
 
-//d08: Varianta příkladu ze skupiny b: Uživatel opakovaně zadá znak, program vždy odpoví, zda se jedná o písmeno,
-//číslici nebo jiný znak. Zadávání končí zadáním prázdného znaku (tedy jen ENTER). Řešte pomocí } else { if.
+            char znak;  //ve Visual studiu by se použil typ Char
 
-main_reseni()
-znak string  //ve Visual studiu by se použil typ Char
+            do {
+                Console.WriteLine("Zadej znak:", "Druh znaku [číslo,písmeno,jiný]");
+                znak = Convert.ToChar(Console.ReadLine());
+                if (znak != null) {
+                    if (znak >= 97 && znak <= 122) {   //druhá možnost
+                                                           //if znak >= "a"  && znak <= "z" {
+                        Console.WriteLine("znak je malé písmeno");
+                    }
+                    else if (znak >= 'A' && znak <= 'Z') {
+                        Console.WriteLine("znak je velké písmeno");
+                    }
+                    else if (znak >= '0' && znak <= '9') {
+                        Console.WriteLine("znak je číslo");
+                    }
+                    else {
+                        Console.WriteLine("znak není písmeno ani číslo");
+                    }
+                }
+            }
+            while (znak != null);
+        }
 
-Do
-  znak = Console.WriteLine(Zadej znak:", "Druh znaku [číslo,písmeno,jiný]")
-  if znak<> \" {
-    ifc(znak) >= 97  &&c(znak) <= 122 {   //druhá možnost
-    //if znak >= "a"  && znak <= "z" {
-      Console.WriteLine("znak je malé písmeno", , "vysledek"
-    } else {if znak >= "A"  && znak <= "Z" {
-      Console.WriteLine("znak je velké písmeno", , "vysledek"
-    } else {if znak >= "0"  && znak <= "9" {
-      Console.WriteLine("znak je číslo", , "vysledek"
-    } else {
-      Console.WriteLine("znak není písmeno ani číslo", , "vysledek"
-    }
-  }
-Loop Until znak = \"
+        public static void Mainx_reseni_b() {
+            //d08: Varianta příkladu ze skupiny b: Uživatel opakovaně zadá znak, program vždy odpoví, zda se jedná o písmeno,
+            //číslici nebo jiný znak. Zadávání končí zadáním prázdného znaku (tedy jen ENTER). Řešte pomocí } else { if.
 
+            //Tato verze je přehlednější, neboť v ní není vnořená podmínka
+            string s;   //ve Visual studiu by se použil typ Char
+            char znak;
 
-//d08: Varianta příkladu ze skupiny b: Uživatel opakovaně zadá znak, program vždy odpoví, zda se jedná o písmeno,
-//číslici nebo jiný znak. Zadávání končí zadáním prázdného znaku (tedy jen ENTER). Řešte pomocí } else { if.
+            do {
+                Console.WriteLine("Zadej znak:", "Druh znaku [číslo,písmeno,jiný]");
+                znak = Convert.ToChar(Console.ReadLine());
 
-main_reseni_b()  //Tato verze je přehlednější, neboť v ní není vnořená podmínka
-znak string, s string  //ve Visual studiu by se použil typ Char
+                if (znak >= 'a' && znak <= 'z') {
+                    s = "znak je malé písmeno";
+                }
+                else if (znak >= 'A' && znak <= 'Z') {
+                    s = "znak je velké písmeno";
+                }
+                else if (znak >= '0' && znak <= '9') {
+                    s = "znak je číslo";
+                }
+                else {
+                    s = "znak není písmeno ani číslo";
+                }
+                if (znak != null) {
+                    Console.WriteLine($"{s}");
+                }while (znak != null);
+            } while (znak != null);
 
-Do
-  znak = Console.WriteLine(Zadej znak:", "Druh znaku [číslo,písmeno,jiný]")
-
-
-  if znak >= "a"  && znak <= "z" {
-      s = "znak je malé písmeno"
-  } else {if znak >= "A"  && znak <= "Z" {
-      s = "znak je velké písmeno"
-  } else {if znak >= "0"  && znak <= "9" {
-      s = "znak je číslo"
-  } else {
-      s = "znak není písmeno ani číslo"
-  }
-
-
-  if znak <> \" { MsgBox s, , "výsledek"
-Loop Until znak = \"
-
-
-
+        }
     }
 }
--- D/d09_teplota_vody.cs
+++ D/d09_teplota_vody.cs
@@ -4,84 +4,93 @@ using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 using Microsoft.VisualBasic.FileIO;
 
 namespace D {
+
     internal class d09_teplota_vody {
-        
 
-//9) Uživatel bude zadávat postupně teploty vody s přesností na desetinu stupně.
-//Zadávání ukončí zadáním teploty přesahující 100° (tato teplota je ale také součástí dat!).
-//Zadávání končí, protože pak už voda vyvřela.
-//Program zobrazí zadané teploty, minimální teplotu a kolikátá v pořadí tato teplota byla.
-//A také průměr teplot
-//Moje poznámky: pořadí jsem mohl vyřešit úplně jednoduše a nemusel jsem do toho tahat proměnnou "i", nedošlo mi, že už by nemělo být součásti dat teplota více než 100,
+        public static void Mainx() {
+            //9) Uživatel bude zadávat postupně teploty vody s přesností na desetinu stupně.
+            //Zadávání ukončí zadáním teploty přesahující 100° (tato teplota je ale také součástí dat!).
+            //Zadávání končí, protože pak už voda vyvřela.
+            //Program zobrazí zadané teploty, minimální teplotu a kolikátá v pořadí tato teplota byla.
+            //A také průměr teplot
+            //Moje poznámky: pořadí jsem mohl vyřešit úplně jednoduše a nemusel jsem do toho tahat proměnnou "i", nedošlo mi, že už by nemělo být součásti dat teplota více než 100,
 
+            float zadanaTeplota, minTeplota, soucetTeplot;
+            string zadaneTeploty;
+            byte i;
+            int poradi;
+            minTeplota = 0;
+            poradi = 0;
+            soucetTeplot = 0;
 
-    zadanaTeplota float, minTeplota float, soucetTeplot float
-    zadaneTeploty string
-    i byte
-    poradi int
+            zadaneTeploty = "";
+            i = 0;
+            do {
+                Console.WriteLine("Zadejte postupně teplotu vody na desetinu stupně.\nJakmile voda vyvře, tak se ukončím.");
+                zadanaTeplota = float.Parse(Console.ReadLine());
+                zadaneTeploty += $"{Math.Round(zadanaTeplota, 1)}, ";
+                if (i == 0) {
+                    minTeplota = zadanaTeplota; //inizializace teploty
+                    poradi++;
+                }
+                else {
+                    if (zadanaTeplota < minTeplota) {
+                        minTeplota = zadanaTeplota;
+                        poradi++;
+                    }
+                }
+                soucetTeplot += zadanaTeplota;
+                i++;
+            }
+            while (soucetTeplot <= 100);
 
-
-    zadaneTeploty = \"
-    i = 0
-    Do
-    zadanaTeplota = Console.WriteLine(Zadejte postupně teplotu vody na desetinu stupně.\nJakmile voda vyvře, tak se ukončím.")
-    zadaneTeploty = zadaneTeploty + Round(zadanaTeplota, 1)) + ","
-    if i = 0 {
-        minTeplota = zadanaTeplota //inizializace teploty
-        poradi = i + 1
-    } else {if zadanaTeplota<minTeplota {
-        minTeplota = zadanaTeplota
-        poradi = i + 1
-    }
-    soucetTeplot = soucetTeplot + zadanaTeplota
-    i = i + 1
-    Loop Until zadanaTeplota >= 100
-
-
-    MsgBox ("Zadané hodnoty:" + zadaneTeploty + Chr(10) + 
-            "Minimální teplota:" + Round(minTeplota, 1)) + Chr(10) + 
-            "Pořadí minimální teploty:" + poradi)) + Chr(10) + 
-            "Průměr teplot:" + Round(soucetTeplot / i, 1)) + " (" + soucetTeplot) + " /" + i) + " )"
-
-
-
-//Řešení:
-
-//d09) Uživatel bude zadávat postupně teploty s přesností na desetinu stupně.
-//Zadávání ukončí zadáním teploty přesahující 100°
-//(tato teplota je ale také součástí dat!). Program zobrazí zadané teploty, minimální teplotu
-//a kolikátá v pořadí tato teplota byla. A také průměr teplot
-main_reseni()
-    teplota float, minT float, sum float
-    i byte, poradi byte
-    s string
-
-
-    poradi = 0: i = 0: sum = 0
-    Do
-        teplota = Console.WriteLine(Zadej teplotu s přesností na desetinu stupně, teplota vetsi nez 100 zadavani ukonci")
-        if i = 0 {
-            minT = teplota        //lepší by bylo inicializovat minimum před smyčkou
-            poradi = 1
+            Console.WriteLine($"Zadané hodnoty: {zadaneTeploty}\n" +
+                              $"Minimální teplota: {Math.Round(minTeplota, 1)}\n" +
+                              $"Pořadí minimální teploty: {poradi}\n" +
+                              $"Průměr teplot: {Math.Round(soucetTeplot / i, 1)}");
         }
-        //if teplota <= 100 {             //toto ne, protože i tentokrát poslední hodnota patří do platných dat
-        s = s + Chr(10) + teplota)
-        i = i + 1
-        sum = sum + teplota
-        if teplota<minT {
-            minT = teplota
-            poradi = i
+
+        public static void Mainx_reseni() {
+            //Řešení:
+
+            //d09) Uživatel bude zadávat postupně teploty s přesností na desetinu stupně.
+            //Zadávání ukončí zadáním teploty přesahující 100°
+            //(tato teplota je ale také součástí dat!). Program zobrazí zadané teploty, minimální teplotu
+            //a kolikátá v pořadí tato teplota byla. A také průměr teplot
+            float teplota, minT, sum;
+            byte i, poradi;
+            string s;
+
+            poradi = 0;
+            i = 0;
+            sum = 0;
+            s = "";
+            minT = 0;
+            do {
+                Console.WriteLine("Zadej teplotu s přesností na desetinu stupně, teplota vetsi nez 100 zadavani ukonci");
+                teplota = float.Parse(Console.ReadLine());
+                if (i == 0) {
+                    minT = teplota;      //lepší by bylo inicializovat minimum před smyčkou
+                    poradi = 1;
+                }
+                //if teplota <= 100 {             //toto ne, protože i tentokrát poslední hodnota patří do platných dat
+                s += $"\n{teplota}";
+                i++;
+                sum += teplota;
+                if (teplota < minT) {
+                    minT = teplota;
+                    poradi = i;
+                }
+            }
+            //}
+            while (teplota < 100);  //nebo }  while ( teplota <= 100
+
+            s = $"Zadané teploty: {s}\nNejnižší je {minT}° a je {poradi} v pořadí.";
+            Console.WriteLine($"{s}\nprůměrná teplota: {Math.Round(sum / i, 1)}");
         }
-        //}
-    Loop Until teplota > 100    //nebo Loop While teplota <= 100
-    
-    s = "Zadané teploty:" + s + Chr(10) + "Nejnižší je " + minT) + "° a je " + poradi) + ". v pořadí."
-    MsgBox s + Chr(10) + "průměrná teplota: " + Round(sum / i, 1))
-    
-
-
     }
 }
