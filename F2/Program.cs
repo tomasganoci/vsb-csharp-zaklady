5674e8dc727da4ad86c30b91b33884f97af09bc3
Tomáš Gánoci <tomas.ganoci@gmail.com>
2025-02-09 00:31:55 +0100

    zkouška

--- F2/Program.cs
+++ F2/Program.cs
@@ -7,28 +7,31 @@ using System.Threading.Tasks;
 namespace F2 {
     internal class Program {
         static void Main(string[] args) {
-            f2_01_uroceni.Mainx();
-            f2_01_uroceni.Mainx_reseni();
-            f2_02_sportka_bez_duplicit.Mainx();
-            f2_02_sportka_bez_duplicit.Mainx_reseni();
-            f2_03_subrutina.Mainx();
-            f2_03_subrutina.Mainx_reseni();
-            f2_04_pocet_vyskytu.Mainx();
-            f2_04_pocet_vyskytu.Mainx_reseni();
-            f2_05_pocet_padlych.Mainx();
-            f2_05_pocet_padlych.Mainx_reseni();
-            f2_06_suda_cisla_z_pole.Mainx();
-            f2_06_suda_cisla_z_pole.Mainx_reseni();
-            f2_07_serazeni_cisel_bool.Mainx();
-            f2_07_serazeni_cisel_bool.Mainx_reseni();
-            f2_08_poruseni_serazeni.Mainx();
-            f2_08_poruseni_serazeni.Mainx_reseni();
-            f2_09_linearni_aritmeticka_posloupnost.Mainx();
-            f2_09_linearni_aritmeticka_posloupnost.Mainx_reseni();
-            f2_10_serazene_cisla.Mainx();
-            f2_10_serazene_cisla.Mainx_reseni();
-            f2_11_sifrovani_znaku.Mainx();
-            f2_12_sifrovani_slov.Mainx();
+            //f2_01_uroceni.Mainx();
+            //f2_01_uroceni.Mainx_reseni();
+            //f2_02_sportka_bez_duplicit.Mainx();
+            //f2_02_sportka_bez_duplicit.Mainx_reseni();
+            //f2_03_subrutina.Mainx();
+            //f2_03_subrutina.Mainx_reseni();
+            //f2_04_pocet_vyskytu.Mainx();
+            //f2_04_pocet_vyskytu.Mainx_reseni();
+            //f2_05_pocet_padlych.Mainx();
+            //f2_05_pocet_padlych.Mainx_reseni();
+            //f2_06_suda_cisla_z_pole.Mainx();
+            //f2_06_suda_cisla_z_pole.Mainx_reseni();
+            //f2_07_serazeni_cisel_bool.Mainx();
+            //f2_07_serazeni_cisel_bool.Mainx_reseni();
+            //f2_08_poruseni_serazeni.Mainx();
+            //f2_08_poruseni_serazeni.Mainx_reseni();
+            //f2_09_linearni_aritmeticka_posloupnost.Mainx();
+            //f2_09_linearni_aritmeticka_posloupnost.Mainx_reseni();
+            //f2_10_serazene_cisla.Mainx();
+            //f2_10_serazene_cisla.Mainx_reseni();
+            //f2_11_sifrovani_znaku.Mainx();
+            //f2_12_sifrovani_slov.Mainx();
+            zkouska.Mainx();
+            zkouska.Main_f();
+            zkouska.Main_f1();
         }
     }
 }
---
+++ F2/zkouska.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F2 {
+
+    internal class zkouska {
+
+        public static void Mainx() {
+            int[] zadaneCisla = new int[100];
+            int zadaneCislo = 0;
+            int i = 1;
+            int pocetZadanych = 0; 
+            int soucetLichych = 0;
+            int pocetLichych = 0;
+            double prumerLichych;
+            do {
+                Console.WriteLine("Zadejte číslo a já ti na konci zobrazím průměr lichých čísel v pozici");
+                zadaneCislo = int.Parse(Console.ReadLine()); 
+                if (zadaneCislo == 0) {
+                    break;
+                }
+                zadaneCisla[i] = zadaneCislo;
+                i++;
+                pocetZadanych++;
+            } while (zadaneCislo != 0);
+
+            for (i = 1; i <= pocetZadanych; i+=2) {
+                soucetLichych += zadaneCisla[i];
+                pocetLichych++;
+            }
+            prumerLichych = (double)soucetLichych / pocetLichych;
+            Console.WriteLine($"Průměr lychých čísel je {prumerLichych:f}");
+        }
+
+        public static void Main_f() {
+
+            int[] poleCisel = new int[100];
+            int zadaneCislo = 0;
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < poleCisel.Length; i++) {
+                zadaneCislo = rnd.Next(-100,100);
+                poleCisel[i] = zadaneCislo;
+            }
+            Console.WriteLine($"Nejmenší číslo ze zadání je {FunkceNejmensi(poleCisel)}");
+        }
+        private static int FunkceNejmensi(int[] cislo) {
+            int nejmensiCislo = cislo[0];
+            for (int i = 0; i < cislo.Length; i++) {
+                if (cislo[i] < nejmensiCislo) {
+                    nejmensiCislo = cislo[i];
+                }
+            }
+            return nejmensiCislo;
+        }
+
+
+        public static void Main_f1() {
+            int[] poleCisel = new int[100];
+            int zadaneCislo = 0;
+            int vysledekNejmensiCislo = 0;
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < poleCisel.Length; i++) {
+                zadaneCislo = rnd.Next(-100, 100);
+                poleCisel[i] = zadaneCislo;
+            }
+            FunkceNejmensiSubrutina(poleCisel, out vysledekNejmensiCislo);
+            Console.WriteLine($"Nejmenší číslo ze zadání je {vysledekNejmensiCislo}");
+        }
+        private static void FunkceNejmensiSubrutina(int[] cisla, out int nejmensiCislo) {
+            nejmensiCislo = cisla[0];
+            for (int i = 0; i < cisla.Length; i++) {
+                if (cisla[i] < nejmensiCislo) {
+                    nejmensiCislo = cisla[i];
+                }
+            }
+        }
+    }
+}
